5f8468216225346965b474304ea7d4692bc257c2
FJ98 <felix.solano@utec.edu.pe>
2019-05-03 13:13:05 -0500

    Biblioteca hecho en clase

---
+++ Biblioteca/cmake-build-debug/CMakeFiles/Biblioteca.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Biblioteca.dir/main.cpp.o"
+  "Biblioteca.pdb"
+  "Biblioteca.exe"
+  "libBiblioteca.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Biblioteca.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
